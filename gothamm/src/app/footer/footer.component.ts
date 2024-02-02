b932cbad5a345f79db7717e83553253ceef9dba2
mariembenhassen <121687274+mariembenhassen@users.noreply.github.com>
2024-02-02 18:59:41 +0100

    footer

--- gothamm/src/app/footer/footer.component.ts
+++ gothamm/src/app/footer/footer.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+//import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+//import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-footer',
+  //standalone: true,
+  //imports: [FontAwesomeModule], // alternatively, individual components can be imported
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
+ 
+ // faCoffee = faCoffee;
 
 }
